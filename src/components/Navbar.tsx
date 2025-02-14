4cfdb8fbf72775624cf8b15465e2f932d2ad8f6c
Andres Fritsche <andresf0090@gmail.com>
2025-02-13 22:42:47 -0400

    added variable colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,17 +7,17 @@ import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <nav className='w-screen flex  justify-evenly items-center px-5 py-2 '>
+    <nav className='w-screen flex justify-evenly items-center px-5 py-2 '>
         
         <Image src="/Next_Design__1_-removebg-preview.png" width={200} height={150} alt="image" className=''/>
         {links.map((link) => {
             const {id, url, text} = link
             return <a key={id} href={url} className='font-bold text-xl'>{text}</a>
         })}
-        <div className='text-3xl flex gap-5'>
+        <div className='text-3xl flex gap-5 text-[--primary]'>
             {social.map((socialIcon) => {
                 const {id, url, icon,} = socialIcon
-                return <a key={id} href={url} className=''>{icon}</a>
+                return <a key={id} href={url} className='text-'>{icon}</a>
             })}
         </div>
     </nav>
