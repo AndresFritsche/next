5f1c2289ef8fc473695c86612c4e5d7f3b309fe0
Andres Fritsche <andresf0090@gmail.com>
2025-02-14 12:11:15 -0400

    added missing routes

---
+++ src/app/desarrollo-web/page.tsx
@@ -0,0 +1,5 @@
+export default function DesarrolloWeb() {
+  return (
+    <div>DesarrolloWeb</div>
+  )
+}
---
+++ src/app/servicios/page.tsx
@@ -0,0 +1,9 @@
+export default function Servicios() {
+  return (
+    <section>
+        <div className="pt-24 flex flex-col justify-center items-center">
+            <h1>asdasdasda</h1>
+        </div>
+    </section>
+  )
+}
