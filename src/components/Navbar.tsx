efcfc139089f82bf8ef87c0daf8eff8f5333d07b
Andres Fritsche <andresf0090@gmail.com>
2025-02-13 22:04:32 -0400

    added poppins font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,12 +12,12 @@ const Navbar = () => {
         <Image src="/Next_Design__1_-removebg-preview.png" width={200} height={150} alt="image" className=''/>
         {links.map((link) => {
             const {id, url, text} = link
-            return <a key={id} href={url} className=''>{text}</a>
+            return <a key={id} href={url} className='font-bold text-xl'>{text}</a>
         })}
         <div className='text-3xl flex gap-5'>
             {social.map((socialIcon) => {
                 const {id, url, icon,} = socialIcon
-                return <a key={id} href={url} className='w-1/2'>{icon}asd</a>
+                return <a key={id} href={url} className='w-1/2'>{icon}</a>
             })}
         </div>
     </nav>
