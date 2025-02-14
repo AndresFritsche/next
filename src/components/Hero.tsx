b0b9398c0dfc9bbdeb94027714339b55dbc1def5
Andres Fritsche <andresf0090@gmail.com>
2025-02-14 07:37:25 -0400

    added hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,29 @@
+import Image from 'next/image'
+
 const Hero = () => {
   return (
-    <main>
-        <div>
-            <h1 className='text-center text-4xl font-bold text-[--primary] mt-10'>Welcome to Next.js</h1>
-            <p className='text-center font-extralight text-xl mt-5'>A React Framework For Production</p>
-        </div>
-    </main>
+    <main className='w-screen min-h-screen flex flex-col justify-center items-center p-8 gap-12 bg-gradient-to-b from-[--background-start] to-[--background-end]'>
+    {/* Text Section */}
+    <div className="w-full max-w-4xl flex flex-col justify-center items-center gap-6 text-center">
+      <h1 className='text-5xl md:text-6xl font-bold text-[--primary]'>
+        Soluciones Web Modernas para el Éxito de tu Negocio
+      </h1>
+      <p className='text-xl md:text-2xl font-light text-[--secondary] mt-4'>
+      Creamos sitios web rápidos, modernos y de alta conversión que generan resultados.
+      </p>
+    </div>
+  
+    {/* Image Section */}
+    <div className='w-full max-w-6xl flex justify-center items-center p-6'>
+      <Image
+        src="/undraw_responsive_csbt (2).svg"
+        width={1200}
+        height={600}
+        alt="Responsive Web Design Illustration"
+        className='w-full h-auto'
+      />
+    </div>
+  </main>
   )
 }
-export default Hero;
+export default Hero
