09745f28e8f9d8f0753256efaa6c415a2c3bf515
Andres Fritsche <andresf0090@gmail.com>
2025-02-15 01:02:00 -0400

    added features section to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 
@@ -8,6 +9,7 @@ export default function Home() {
     <div>
       <Hero />
       <Services />
+      <Features />
       
     </div>
   );
